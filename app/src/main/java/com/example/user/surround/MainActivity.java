d8945ccb79e7b3546d0447ee14b846f4b3ca8c92
karthikrana <karthikflyer007@gmail.com>
2018-03-21 18:57:02 +0530

    Added Login and Signup UI

--- app/src/main/java/com/example/user/surround/MainActivity.java
+++ app/src/main/java/com/example/user/surround/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.user.surround;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+    public static FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (mAuth.getCurrentUser() == null) {
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        // updateUI(currentUser);
     }
 }
